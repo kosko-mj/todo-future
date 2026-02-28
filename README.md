0ced50b26e3312045c52847d710955f07538760d
Matthew <252770322+kosko-mj@users.noreply.github.com>
2026-02-28 16:02:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webpack Starter Template
+
+My personal starter for basic web projects.
+
+## Usage
+
+1. Clone this template
+2. `npm install`
+3. `npm start` - dev server
+4. `npm run build` - production build
+
+## Features
+- Webpack 5
+- Dev server with hot reload
+- CSS support
+- Image handling
+- HTML template with auto-injected script
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    watchFiles: ['./src/index.html'],
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][hash][ext]',
+        },
+      },
+    ],
+  },
+};
